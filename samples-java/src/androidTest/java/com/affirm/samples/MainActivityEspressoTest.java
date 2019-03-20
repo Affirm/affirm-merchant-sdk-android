118e011dbc6eac76a1e8d14af844e6d213ce2fb0
evan <liusuzheng2012@gmail.com>
2019-03-20 15:35:38 +0800

    some ui test

--- samples-java/src/androidTest/java/com/affirm/samples/MainActivityEspressoTest.java
+++ samples-java/src/androidTest/java/com/affirm/samples/MainActivityEspressoTest.java
@@ -8,6 +8,7 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.TimeUnit;
 
+import androidx.test.espresso.web.webdriver.DriverAtoms;
 import androidx.test.espresso.web.webdriver.Locator;
 import androidx.test.filters.LargeTest;
 import androidx.test.rule.ActivityTestRule;
@@ -21,8 +22,10 @@ import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static androidx.test.espresso.web.assertion.WebViewAssertions.webMatches;
 import static androidx.test.espresso.web.sugar.Web.onWebView;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.clearElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.findElement;
 import static androidx.test.espresso.web.webdriver.DriverAtoms.getText;
+import static androidx.test.espresso.web.webdriver.DriverAtoms.webClick;
 import static org.hamcrest.CoreMatchers.startsWith;
 import static org.hamcrest.core.StringContains.containsString;
 
@@ -32,6 +35,28 @@ public class MainActivityEspressoTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void testPromoModal() {
+        SystemClock.sleep(1000);
+
+        onView(withId(R.id.productModalButton)).perform(click());
+
+        onWebView().withTimeout(20, TimeUnit.SECONDS);
+
+        SystemClock.sleep(8000);
+
+        pressBack();
+
+        onView(withId(R.id.siteModalButton)).perform(click());
+
+        onWebView().withTimeout(20, TimeUnit.SECONDS);
+
+        SystemClock.sleep(8000);
+
+        pressBack();
+    }
+
 
     @Test
     public void testPromo() {
@@ -48,6 +73,27 @@ public class MainActivityEspressoTest {
         onWebView().withTimeout(20, TimeUnit.SECONDS)
                 .withElement(findElement(Locator.ID, "prequal-header"))
                 .check(webMatches(getText(), containsString("Make easy monthly payments over 3, 6, or 12 months")));
+
+        SystemClock.sleep(2000);
+
+        pressBack();
+    }
+
+    @Test
+    public void testCheckout() {
+        SystemClock.sleep(1000);
+
+        onView(withId(R.id.checkout)).perform(click());
+
+        onWebView().withTimeout(20, TimeUnit.SECONDS);
+
+        SystemClock.sleep(2000);
+
+        onWebView().withElement(findElement(Locator.ID, "input-phoneNumber"))
+                .perform(clearElement())
+                .perform(DriverAtoms.webKeys("8888888888"))
+                .withElement(findElement(Locator.ID, "account-submit"))
+                .perform(webClick());
 
         SystemClock.sleep(2000);
 

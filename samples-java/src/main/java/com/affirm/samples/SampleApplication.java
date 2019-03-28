ce0ea0e027a594508bed2a531bfe456746eef74b
evan <liusuzheng2012@gmail.com>
2019-03-28 16:23:27 +0800

    Remove LeakCanary

--- samples-java/src/main/java/com/affirm/samples/SampleApplication.java
+++ samples-java/src/main/java/com/affirm/samples/SampleApplication.java
@@ -3,15 +3,12 @@ package com.affirm.samples;
 import android.app.Application;
 
 import com.affirm.android.Affirm;
-import com.squareup.leakcanary.LeakCanary;
 
 public class SampleApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        LeakCanary.install(this);
 
         Affirm.initialize(new Affirm.Configuration.Builder("Y8CQXFF044903JC0", Affirm.Environment.SANDBOX)
                 .setMerchantName(null)
--- samples-kotlin/src/main/java/com/affirm/sampleskt/SampleApplication.kt
+++ samples-kotlin/src/main/java/com/affirm/sampleskt/SampleApplication.kt
@@ -1,16 +1,12 @@
 package com.affirm.sampleskt
 
 import android.app.Application
-
 import com.affirm.android.Affirm
-import com.squareup.leakcanary.LeakCanary
 
 class SampleApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-
-        LeakCanary.install(this)
 
         Affirm.initialize(Affirm.Configuration.Builder("Y8CQXFF044903JC0", Affirm.Environment.SANDBOX)
                 .setMerchantName(null)

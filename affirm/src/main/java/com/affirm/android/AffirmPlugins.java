6af079564bc5285b78159a23a69449fe700d85f0
evan <liusuzheng2012@gmail.com>
2019-04-16 15:12:19 +0800

    Enable TLS1.2 on devices < 22
    https://stackoverflow.com/questions/37006416/sslexception-connection-closed-by-peer-on-android-4-x-versions
    https://github.com/square/okhttp/issues/2372

--- affirm/src/main/java/com/affirm/android/AffirmPlugins.java
+++ affirm/src/main/java/com/affirm/android/AffirmPlugins.java
@@ -1,17 +1,40 @@
 package com.affirm.android;
 
+import android.app.Application;
+import android.content.Context;
+import android.os.Build;
+
 import com.affirm.android.model.AffirmAdapterFactory;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.security.ProviderInstaller;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.security.KeyStore;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.TrustManagerFactory;
+import javax.net.ssl.X509TrustManager;
+
 import androidx.annotation.NonNull;
+import okhttp3.ConnectionSpec;
 import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.TlsVersion;
 
 class AffirmPlugins {
 
@@ -106,8 +129,143 @@ class AffirmPlugins {
             clientBuilder.connectTimeout(5, TimeUnit.SECONDS);
             clientBuilder.readTimeout(30, TimeUnit.SECONDS);
             clientBuilder.followRedirects(false);
-            mRestClient = AffirmHttpClient.createClient(clientBuilder);
+
+            mRestClient = AffirmHttpClient.createClient(enableTls12OnPreLollipop(clientBuilder));
         }
         return mRestClient;
+    }
+
+    /**
+     * Enables TLS v1.2 when creating SSLSockets.
+     * <p/>
+     * For some reason, android supports TLS v1.2 from API 16, but enables it by
+     * default only from API 20.
+     *
+     * @link https://developer.android.com/reference/javax/net/ssl/SSLSocket.html
+     * @see SSLSocketFactory
+     */
+    private class Tls12SocketFactory extends SSLSocketFactory {
+        private final String[] tlsV12Only = {"TLSv1.2"};
+
+        final SSLSocketFactory delegate;
+
+        Tls12SocketFactory(SSLSocketFactory base) {
+            this.delegate = base;
+        }
+
+        @Override
+        public String[] getDefaultCipherSuites() {
+            return delegate.getDefaultCipherSuites();
+        }
+
+        @Override
+        public String[] getSupportedCipherSuites() {
+            return delegate.getSupportedCipherSuites();
+        }
+
+        @Override
+        public Socket createSocket(Socket s, String host, int port, boolean autoClose)
+                throws IOException {
+            return patch(delegate.createSocket(s, host, port, autoClose));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port) throws IOException {
+            return patch(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(String host, int port, InetAddress localHost, int localPort)
+                throws IOException {
+            return patch(delegate.createSocket(host, port, localHost, localPort));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress host, int port) throws IOException {
+            return patch(delegate.createSocket(host, port));
+        }
+
+        @Override
+        public Socket createSocket(InetAddress address,
+                                   int port,
+                                   InetAddress localAddress,
+                                   int localPort) throws IOException {
+            return patch(delegate.createSocket(address, port, localAddress, localPort));
+        }
+
+        private Socket patch(Socket s) {
+            if (s instanceof SSLSocket) {
+                ((SSLSocket) s).setEnabledProtocols(tlsV12Only);
+            }
+            return s;
+        }
+    }
+
+    private OkHttpClient.Builder enableTls12OnPreLollipop(OkHttpClient.Builder client) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP_MR1) {
+
+            Context context = null;
+            try {
+                context = getApplicationUsingReflection();
+                ProviderInstaller.installIfNeeded(context);
+            } catch (GooglePlayServicesRepairableException e) {
+
+                // Indicates that Google Play services is out of date, disabled, etc.
+
+                // Prompt the user to install/update/enable Google Play services.
+                if (context != null) {
+                    GoogleApiAvailability.getInstance()
+                            .showErrorNotification(context,
+                                    e.getConnectionStatusCode());
+                }
+
+                return client;
+
+            } catch (GooglePlayServicesNotAvailableException e) {
+                // Indicates a non-recoverable error; the ProviderInstaller is not able
+                // to install an up-to-date Provider.
+                AffirmLog.e("Could not install providers", e);
+                return client;
+            } catch (Exception e) {
+                AffirmLog.e("Could not install providers", e);
+            }
+
+            try {
+                TrustManagerFactory trustManagerFactory = TrustManagerFactory.getInstance(
+                        TrustManagerFactory.getDefaultAlgorithm());
+                trustManagerFactory.init((KeyStore) null);
+                TrustManager[] trustManagers = trustManagerFactory.getTrustManagers();
+                if (trustManagers.length != 1 || !(trustManagers[0] instanceof X509TrustManager)) {
+                    throw new IllegalStateException("Unexpected default trust managers:"
+                            + Arrays.toString(trustManagers));
+                }
+                X509TrustManager trustManager = (X509TrustManager) trustManagers[0];
+
+                SSLContext sc = SSLContext.getInstance("TLSv1.2");
+                sc.init(null, null, null);
+                client.sslSocketFactory(
+                        new Tls12SocketFactory(sc.getSocketFactory()), trustManager);
+
+                ConnectionSpec cs = new ConnectionSpec.Builder(ConnectionSpec.MODERN_TLS)
+                        .tlsVersions(TlsVersion.TLS_1_2)
+                        .build();
+
+                List<ConnectionSpec> specs = new ArrayList<>();
+                specs.add(cs);
+                specs.add(ConnectionSpec.COMPATIBLE_TLS);
+                specs.add(ConnectionSpec.CLEARTEXT);
+
+                client.connectionSpecs(specs);
+            } catch (Exception exc) {
+                AffirmLog.e("Error while setting TLS 1.2", exc);
+            }
+        }
+
+        return client;
+    }
+
+    private Application getApplicationUsingReflection() throws Exception {
+        return (Application) Class.forName("android.app.ActivityThread")
+                .getMethod("currentApplication").invoke(null, (Object[]) null);
     }
 }

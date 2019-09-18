4b6897e8672db4e8e9fd42e8497ad3e929b7c44b
Evan <liusuzheng2012@gmail.com>
2019-09-18 14:20:51 +0800

    More detailed affirm error message

--- affirm/src/main/java/com/affirm/android/exception/InvalidRequestException.java
+++ affirm/src/main/java/com/affirm/android/exception/InvalidRequestException.java
@@ -1,33 +1,46 @@
 package com.affirm.android.exception;
 
-
 import com.affirm.android.model.AffirmError;
-
 import androidx.annotation.Nullable;
+
+import java.util.List;
 
 public class InvalidRequestException extends AffirmException {
 
     @Nullable
-    private final String mType;
+    private final String type;
     @Nullable
-    private final String mField;
+    private final String field;
+    @Nullable
+    private final List<String> fields;
 
-    public InvalidRequestException(@Nullable String message, @Nullable String type,
-                                   @Nullable String field, @Nullable String requestId,
-                                   @Nullable Integer statusCode, @Nullable AffirmError affirmError,
+    public InvalidRequestException(@Nullable String message,
+                                   @Nullable String type,
+                                   @Nullable List<String> fields,
+                                   @Nullable String field,
+                                   @Nullable String requestId,
+                                   @Nullable Integer statusCode,
+                                   @Nullable AffirmError affirmError,
                                    @Nullable Throwable e) {
         super(affirmError, message, requestId, statusCode, e);
-        mType = type;
-        mField = field;
+
+        this.type = type;
+        this.field = field;
+        this.fields = fields;
     }
 
     @Nullable
     public String getType() {
-        return mType;
+        return type;
     }
 
     @Nullable
     public String getField() {
-        return mField;
+        return field;
+    }
+
+    @Nullable
+    public List<String> getFields() {
+        return fields;
     }
 }

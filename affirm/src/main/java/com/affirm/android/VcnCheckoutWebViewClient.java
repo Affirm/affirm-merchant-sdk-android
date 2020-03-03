7bdc7b48f0b35c092ddb294c431caaadea252cc6
Evan <liusuzheng2012@gmail.com>
2020-03-03 13:36:06 +0800

    Handling checkout status is invalid

--- affirm/src/main/java/com/affirm/android/VcnCheckoutWebViewClient.java
+++ affirm/src/main/java/com/affirm/android/VcnCheckoutWebViewClient.java
@@ -1,15 +1,19 @@
 package com.affirm.android;
 
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.affirm.android.exception.ConnectionException;
 import com.affirm.android.model.CardDetails;
 import com.affirm.android.model.VcnReason;
 import com.google.gson.Gson;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
-
-import androidx.annotation.NonNull;
 
 import static com.affirm.android.AffirmConstants.AFFIRM_CHECKOUT_CANCELLATION_URL;
 import static com.affirm.android.AffirmConstants.AFFIRM_CHECKOUT_CONFIRMATION_URL;
@@ -20,6 +24,10 @@ final class VcnCheckoutWebViewClient extends AffirmWebViewClient {
     private final String receiveReasonCodes;
     private static final String VCN_CHECKOUT_REGEX = "data=";
     private static final String ENCODING_FORMAT = "UTF-8";
+    private static final String INVALID_CHECKOUT_REDIRECT_URL =
+            "https://sandbox.affirm.com/u/";
+    private static final String INVALID_CHECKOUT_MESSAGE =
+            "Checkout status is in an invalid state.";
 
     VcnCheckoutWebViewClient(@NonNull Gson gson, @NonNull String receiveReasonCodes,
                              @NonNull Callbacks callbacks) {
@@ -27,6 +35,18 @@ final class VcnCheckoutWebViewClient extends AffirmWebViewClient {
         this.receiveReasonCodes = receiveReasonCodes;
         this.callbacks = callbacks;
         this.gson = gson;
+    }
+
+    @Nullable
+    @Override
+    public WebResourceResponse shouldInterceptRequest(WebView view, WebResourceRequest request) {
+        if (request.getUrl().toString().equals(INVALID_CHECKOUT_REDIRECT_URL)) {
+            callbacks.onWebViewError(
+                    new ConnectionException(INVALID_CHECKOUT_MESSAGE)
+            );
+            return null;
+        }
+        return super.shouldInterceptRequest(view, request);
     }
 
     @Override

629cf18c52ab2ae177e2453382e45f126d57c08f
Evan <liusuzheng2012@gmail.com>
2020-11-25 17:29:25 +0800

    Remove Parcelable on CardDetailsInner

--- affirm/src/main/java/com/affirm/android/model/CardDetailsInner.java
+++ affirm/src/main/java/com/affirm/android/model/CardDetailsInner.java
@@ -1,26 +1,8 @@
 package com.affirm.android.model;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 import java.util.Date;
 
-public class CardDetailsInner implements Parcelable {
-
-    public static final Creator<CardDetailsInner> CREATOR = new Creator<CardDetailsInner>() {
-        @Override
-        public CardDetailsInner createFromParcel(Parcel in) {
-            return new CardDetailsInner(
-                    (CardDetails) in.readParcelable(CardDetails.class.getClassLoader()),
-                    (Date) in.readSerializable()
-            );
-        }
-
-        @Override
-        public CardDetailsInner[] newArray(int size) {
-            return new CardDetailsInner[size];
-        }
-    };
+public class CardDetailsInner {
 
     private CardDetails cardDetails;
 
@@ -37,16 +19,5 @@ public class CardDetailsInner implements Parcelable {
     public CardDetailsInner(CardDetails cardDetails, Date expirationDate) {
         this.cardDetails = cardDetails;
         this.expirationDate = (Date) expirationDate.clone();
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeParcelable(cardDetails, flags);
-        dest.writeSerializable(expirationDate);
     }
 }

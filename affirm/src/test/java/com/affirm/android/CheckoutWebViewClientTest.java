f511182515c344b48b4c3252698d31b212eee7dc
Evan <liusuzheng2012@gmail.com>
2021-06-02 15:29:22 +0800

    Fix test issues

--- affirm/src/test/java/com/affirm/android/CheckoutWebViewClientTest.java
+++ affirm/src/test/java/com/affirm/android/CheckoutWebViewClientTest.java
@@ -1,5 +1,6 @@
 package com.affirm.android;
 
+import android.net.Uri;
 import android.os.Build;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -7,18 +8,25 @@ import android.webkit.WebView;
 
 import com.affirm.android.exception.ConnectionException;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(Uri.class)
 public class CheckoutWebViewClientTest {
 
     @Mock
@@ -28,6 +36,23 @@ public class CheckoutWebViewClientTest {
 
     @InjectMocks
     CheckoutWebViewClient affirmWebViewClient;
+
+    @Before
+    public void setup() {
+        mockUriParse();
+    }
+
+    private void mockUriParse() {
+        PowerMockito.mockStatic(Uri.class);
+        PowerMockito.when(Uri.parse(anyString())).then((Answer<Uri>) invocation -> mockUri((String) invocation.getArguments()[0]));
+    }
+
+    private Uri mockUri(final String url) {
+        Uri mockUri = mock(Uri.class);
+        when(mockUri.toString()).thenReturn(url);
+        when(mockUri.getQueryParameter("checkout_token")).then((Answer<String>) invocation -> url.substring(url.indexOf("checkout_token=") + "checkout_token=".length()));
+        return mockUri;
+    }
 
     @Test
     public void shouldOverrideUrlLoading_Confirmation() {
